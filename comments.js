c9e3221197b4e7de2f4c9b629ae4123eabe6b705
NotADev <64313023+NotADev9000@users.noreply.github.com>
2024-09-26 12:54:17 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,26 @@
+// Create a web server
+const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+
+const comments = JSON.parse(fs.readFileSync('./comments.json'));
+
+app.get('/comments', (req, res) => {
+  res.json(comments);
+});
+
+app.post('/comments', (req, res) => {
+  const newComment = req.body;
+  comments.push(newComment);
+  fs.writeFileSync('./comments.json', JSON.stringify(comments, null, 2));
+  res.json(newComment);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
